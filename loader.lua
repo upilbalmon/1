50e7cbde0f3477f28c903de2b3be9ee796c59c4e
upilbalmon <balmonupil@gmail.com>
2025-10-02 11:57:52 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,7 @@ local playerGui = player:WaitForChild("PlayerGui")
 
 -- Daftar URL skrip untuk dijalankan
 local scriptURLs = {
+    "https://raw.githubusercontent.com/upilbalmon/Fish/refs/heads/main/Loader.lua"
     "https://raw.githubusercontent.com/upilbalmon/goblox/refs/heads/main/AUTO%20COIN%20V3.lua",
     "https://raw.githubusercontent.com/upilbalmon/goblox/refs/heads/main/autohatchx10.lua",
     "https://raw.githubusercontent.com/upilbalmon/1/refs/heads/main/perbekalan.lua",
@@ -18,7 +19,7 @@ local scriptURLs = {
     "https://raw.githubusercontent.com/upilbalmon/Gunung/refs/heads/main/tombolW.lua",
 }
 
-local buttonNames = {"CAJ1", "CAJ2", "CP", "GUNUNG", "BM", "TELE", "SPEED", "LOC", "SIZE", "Weater", "TPT", "tombol W",}
+local buttonNames = {"FISH IT", "CAJ1", "CAJ2", "CP", "GUNUNG", "BM", "TELE", "SPEED", "LOC", "SIZE", "Weater", "TPT", "tombol W",}
 
 -- Konfigurasi UI
 local guiSize = UDim2.new(0, 150, 0, 210)
